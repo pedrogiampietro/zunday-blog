169fb10602607445b391d50465d7a326a148ef33
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-08-01 19:40:46 -0300

    :lock: Adicionando Firebase.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import firebase from './firebase'
 
 import Home from './components/Home'
 import Header from './components/Header'
 import './global.css'
 
 class App extends Component {
+
+  state = {
+    firebaseInitialized: false,
+  }
+
+  componentDidMount() {
+    firebase.isInitialized()
+    .then(resultado => {
+      // devolver o úsuario
+      this.setState({ firebaseInitialized: resultado })
+    })
+  }
+
   render() {
-    return(
+    return this.state.firebaseInitialized !== false ? (
       <BrowserRouter>
         <Header />
         <Switch>
           <Route exact path='/' component={Home} />
         </Switch>
       </BrowserRouter>
+    ) : (
+      <h1>Carregando...</h1>
     )
   }
 }
---
+++ src/firebase.js
@@ -0,0 +1,45 @@
+import app from 'firebase/app';
+import 'firebase/database'
+import 'firebase/auth'
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDqonoBKE0k-ESrzlr1a1L_JnivVMbDNOM",
+    authDomain: "reactapp-9c551.firebaseapp.com",
+    databaseURL: "https://reactapp-9c551.firebaseio.com",
+    projectId: "reactapp-9c551",
+    storageBucket: "reactapp-9c551.appspot.com",
+    messagingSenderId: "953361247296",
+    appId: "1:953361247296:web:20e354e7505e9bbb7bc3a5",
+    measurementId: "G-248PSJL6Z2"
+  };
+  
+
+class Firebase {
+    constructor() {
+        app.initializeApp(firebaseConfig);
+    }
+
+    login(email, password) {
+        return app.auth().signInWithEmailAndPassword(email, password)
+    }
+
+    async register(nome, email, password) {
+       await app.auth().createUserWithEmailAndPassword(email, password)
+
+       const uid = app.auth().currentUser.uid
+
+       return app.database.ref('usuarios').child(uid).set({
+           nome: nome
+       })
+    }
+
+    isInitialized() {
+        return new Promise((resolve, reject) => {
+            app.auth().onAuthStateChanged(resolve)
+        })
+    }
+
+}
+
+
+export default new Firebase()
